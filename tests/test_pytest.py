028c18c7d28b57d771fba800119746ee1921c832
ChernyaevAndrew <115479567+ChernyaevAndrew@users.noreply.github.com>
2026-05-20 20:00:33 +0300

    Added tests using PageObject+PageComponent

---
+++ tests/test_pytest.py
@@ -0,0 +1,16 @@
+def test_user_login():
+    print("Testing user login")
+
+class TestUserLogin:
+    def test_1(self):
+        print("Testing user login")
+
+    def test_2(self):
+        print("Testing user login")
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+def test_assert_negative_case():
+    assert (2 - 2) == 5
