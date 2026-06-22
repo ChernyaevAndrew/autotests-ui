336af3685351ff6d5652f0735788250be02fb772
ChernyaevAndrew <115479567+ChernyaevAndrew@users.noreply.github.com>
2026-06-22 22:48:14 +0300

    Update before CI/CD step

---
+++ components/courses/course_view_component.py
@@ -0,0 +1,44 @@
+from components.base_component import BaseComponent
+from playwright.sync_api import Page, expect
+import allure
+from components.courses.course_view_menu_component import CourseViewMenuComponent
+from elements.text import Text
+from elements.image import Image
+
+
+class CourseViewComponent(BaseComponent):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        self.menu = CourseViewMenuComponent(page)
+
+        self.title = Text(page, 'course-widget-title-text', 'Title')
+        self.image = Image(page, 'course-preview-image', 'Preview')
+        self.max_score_text = Text(page, 'course-max-score-info-row-view-text', 'Max score')
+        self.min_score_text = Text(page, 'course-min-score-info-row-view-text', 'Min score')
+        self.estimated_time_text = Text(page, 'course-estimated-time-info-row-view-text', 'Estimated-time')
+
+    @allure.step('Check visible course view at index "{index}"')
+    def check_visible(
+            self,
+            index: int,  # Индекс карточки в списке курсов
+            title: str,  # Ожидаемый заголовок курса
+            max_score: str,  # Ожидаемый максимальный балл
+            min_score: str,  # Ожидаемый минимальный балл
+            estimated_time: str,  # Ожидаемое время прохождения
+    ):
+        self.image.check_visible(nth=index)
+
+        # Обратите внимание на использование метода nth, он позволяет получить локатор по индексу
+        self.title.check_visible(nth=index)
+        self.title.check_have_text(title, nth=index)
+
+
+        self.max_score_text.check_visible(nth=index)
+        self.max_score_text.check_have_text(f'Max score: {max_score}', nth=index)
+
+        self.min_score_text.check_visible(nth=index)
+        self.min_score_text.check_have_text(f'Min score: {min_score}', nth=index)
+
+        self.estimated_time_text.check_visible(nth=index)
+        self.estimated_time_text.check_have_text(f'Estimated time: {estimated_time}', nth=index)
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,11 @@
-from playwright.sync_api import Playwright, Page
+from typing import Pattern
+
+import allure
+from playwright.sync_api import Page, expect
+
+from tools.logger import get_logger
+
+logger = get_logger("BASE_PAGE")
 
 
 class BasePage:
@@ -6,7 +13,22 @@ class BasePage:
         self.page = page
 
     def visit(self, url: str):
-        self.page.goto(url, wait_until='networkidle')
+        step = f'Opening the url "{url}"'
+
+        with allure.step(step):
+            logger.info(step)
+            self.page.goto(url, wait_until='networkidle')
 
     def reload(self):
-        self.page.reload(wait_until='networkidle')
+        step = f'Reloading page with url "{self.page.url}"'
+
+        with allure.step(step):
+            logger.info(step)
+            self.page.reload(wait_until='domcontentloaded')
+
+    def check_current_url(self, expected_url: Pattern[str]):
+        step = f'Checking that current url matches pattern "{expected_url.pattern}"'
+
+        with allure.step(step):
+            logger.info(step)
+            expect(self.page).to_have_url(expected_url)
